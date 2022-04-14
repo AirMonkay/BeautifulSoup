df80a984c341b6a564f08f910a5ed60ae177828d
Daniel Floyd <shortstack40floyd@gmail.com>
2022-04-14 01:06:54 -0400

    task 6

--- outline.py
+++ outline.py
@@ -1,18 +1,45 @@
 from bs4 import BeautifulSoup
-#import request
+import requests
 import json
 
+jobs = []
 def displayJobDetails():
     print("Display job details")
 
 #function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
 def getJobList(role,location):
     url = 'https://www.indeed.com/jobs?q={role}&l={location}'
-    # Complete the missing part of this function here 
+    url = url.replace("{role}",role)
+    url = url.replace("{location}",location)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,"html.parser")
+    for job in soup.find_all('table',class_='jobCard_mainContent'):
+        jobTitle = job.find('h2','jobTitle').text
+        if jobTitle.startswith('new'):
+            jobTitle = jobTitle.replace('new','')
+        companyName = job.find('span',class_='companyName').text
+        try:
+            description = job.find('div',class_='job-snippet').text
+        except:
+            description = "None"
+        try:
+            salary = job.find('div',class_="salary-snippet-container").text
+        except:
+            salary = "None"
+        jobListing = {
+            'Title':jobTitle,
+            'Company':companyName,
+            'Description':description,
+            'Salary':salary
+        }
+        #print(jobListing)
+        jobs.append(jobListing)
+    return jobs
 
 #save data in JSON file
 def saveDataInJSON(jobDetails):
-    #Complete the missing part of this function here
+    with open("jobDetails.json","w") as file:
+        json.dump(jobDetails,file,indent=4)
     print("Saving data to JSON")
 
 #main function
@@ -23,6 +50,10 @@ def main():
     print("Enter location you want to search")
     location = input()
     print(role+" located in "+location)
+    jobList = getJobList(role,location)
+    print(jobList)
+    saveDataInJSON(jobList)
+    
     # Complete the missing part of this function here
     
 
--- testCode.py
+++ testCode.py
@@ -1,4 +1,4 @@
-import request
+import requests
 
 url = "https://www.indeed.com/jobs?q=Software Developer&l=Charlotte"
 
@@ -7,6 +7,6 @@ headers = {
   'Cookie': 'CTK=1g0it2o1ut5eb800; INDEED_CSRF_TOKEN=D8wIgRyZqdOKHr5eEPTZrpD6cc599zyC; JSESSIONID=3511F5BE47F5AD62E10ECA3D3CB6FD10; PREF="TM=1649901920330:L=Charlotte"; RQ="q=Software+Developer&l=Charlotte&ts=1649901920355"; UD="LA=1649901920:CV=1649901920:TS=1649901920:SG=4247bfd008a1489adc73549b4cb81c01"; ctkgen=1; indeed_rcc=""; jaSerpCount=1'
 }
 
-response = request.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
